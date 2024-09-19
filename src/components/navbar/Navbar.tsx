99f8cf90280930aa115c28d6897cca6727221003
Valtkf <valentin.vgregorio@gmail.com>
2024-09-19 16:55:00 +0200

    ♻️ F0 : refactoring navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -14,60 +14,7 @@ import {
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import SquareeIcon from "../ui/icons/SquareeIcon";
-
-const features: { title: string; href: string; description: string }[] = [
-  {
-    title: "Feature 1",
-    href: "/features/1",
-    description: "Description of feature 1.",
-  },
-  {
-    title: "Feature 2",
-    href: "/features/2",
-    description: "Description of feature 2.",
-  },
-  {
-    title: "Feature 3",
-    href: "/features/3",
-    description: "Description of feature 3.",
-  },
-];
-
-const resources: { title: string; href: string; description: string }[] = [
-  {
-    title: "Resource 1",
-    href: "/resources/1",
-    description: "Description of resource 1.",
-  },
-  {
-    title: "Resource 2",
-    href: "/resources/2",
-    description: "Description of resource 2.",
-  },
-  {
-    title: "Resource 3",
-    href: "/resources/3",
-    description: "Description of resource 3.",
-  },
-];
-
-const company: { title: string; href: string; description: string }[] = [
-  {
-    title: "About Us",
-    href: "/about",
-    description: "Learn more about our company.",
-  },
-  {
-    title: "Careers",
-    href: "/careers",
-    description: "Join our team.",
-  },
-  {
-    title: "Press",
-    href: "/press",
-    description: "Latest news and press releases.",
-  },
-];
+import { company, features, resources } from "@/lib/data-navbar";
 
 export function CustomNavbar() {
   return (
