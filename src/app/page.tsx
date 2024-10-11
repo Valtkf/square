5c150003a5d36fd6784335825cfafc7c222c0801
Valtkf <valentin.vgregorio@gmail.com>
2024-10-11 18:19:37 +0200

    ♻️ F03 : Add cards more responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,16 +15,16 @@ import AnimatedSection from "@/components/ui/animations/AnimatedSection";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="p-8 pb-20">
+      <div className="p-8 pb-20 w-full">
         <header>
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center w-full">
             <AnimatedSection>
               <CustomNavbar />
             </AnimatedSection>
           </div>
         </header>
-        <main className="flex-grow">
-          <div className="flex flex-col justify-center items-center">
+        <main className="flex-grow w-full">
+          <div className="flex flex-col justify-center items-center w-full">
             <AnimatedSection>
               <HeroSection />
             </AnimatedSection>
@@ -33,12 +33,14 @@ export default function Home() {
             </AnimatedSection>
           </div>
         </main>
-        <ChartsSection />
-        <Productivity />
-        <TechnicalStack />
-        <Testimonial />
-        <BilledSection />
-        <Card />
+        <div className="w-full flex flex-col items-center">
+          <ChartsSection />
+          <Productivity />
+          <TechnicalStack />
+          <Testimonial />
+          <BilledSection />
+          <Card />
+        </div>
       </div>
       <FooterSection />
     </div>
